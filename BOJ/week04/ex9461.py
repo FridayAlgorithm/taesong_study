1c56daf13c0f094c8196938bcf444ea1ca6e6cb4
Taesong Kim <taesongweb@gmail.com>
2021-01-23 23:59:28 +0900

    BOJ study DynamicPrograming ex 1904, 9461

--- BOJ/week04/ex9461.py
+++ BOJ/week04/ex9461.py
@@ -1,40 +1,17 @@
-N, M = map(int, input().split())
-graph = [list(input()) for _ in range(N)]  # 입력 받은 체스판
-min_list = []
-white_chess = [
-    ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
-    ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
-    ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
-    ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
-    ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
-    ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
-    ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
-    ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W']
+import sys
 
-]
-
-black_chess = [
-    ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
-    ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
-    ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
-    ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
-    ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
-    ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
-    ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
-    ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B']
-]
-
-
-for i in range(N-7):
-    for j in range(M-7):
-        black_cnt = 0
-        white_cnt = 0
-        for a in range(i, i+8):
-            for b in range(j, j+8):
-                if graph[a][b] != white_chess[a][b]:
-                    white_cnt += 1
-                if graph[a][b] != black_chess[a][b]:
-                    black_cnt += 1
-        min_value = min(white_cnt, black_cnt)
-        min_list.append(min_value)
-print(min(min_list))
+T = int(input())
+for i in range(T):
+    input = sys.stdin.readline()
+    N = int(input)
+    dp = [0] * 101
+    dp[1] = 1
+    dp[2] = 1
+    dp[3] = 1
+    dp[4] = 2
+    dp[5] = 2
+    dp[6] = 3
+    dp[7] = 4
+    for i in range(8, N+1):
+        dp[i] = dp[i-1] + dp[i-5]
+    print(dp[N])
